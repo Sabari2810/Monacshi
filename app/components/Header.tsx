6cac9a6020512b2be8ec471bb25dcc0a310aabe9
Sabari <imsabarimanikandan@gmail.com>
2026-03-19 21:52:02 +0530

    modified to monique

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,11 +12,11 @@ const Header = () => {
     }, []);
 
     return (
-        <div className={`flex font-[open-sans] fixed top-0 left-0 z-50 flex-row w-full items-center justify-between px-10 py-4 transition-all duration-300
+        <div className={`flex font-[open-sans] fixed top-0 left-0 z-50 flex-row w-full items-center justify-between px-20 py-6 transition-all duration-300
   ${scrolled ? "bg-[#191A11] shadow-md" : "bg-transparent"}
 `}>
             <div className='flex items-center max-w-xl justify-between w-full'>
-                <div className='text-5xl'>Monacshi</div>
+                <div className='text-5xl'>Monique</div>
                 <div className='flex cursor-pointer items-center space-x-6'>
                     <NavItem label='Outfits' items={["abc", "fhgfhg", "hgfhgfhg"]} />
                     <NavItem label='Custom Outfits' items={["abc", "fhgfhg", "hgfhgfhg"]} />
@@ -25,7 +25,7 @@ const Header = () => {
             </div>
             <div>
                 <button className='border-2 border-[#F0E4AF] text-[#F0E4AF] hover:bg-[#F0E4AF] 
-          hover:text-[#27281B] border-solid px-10 py-2 cursor-pointer transition ease-in-out 
+          hover:text-[#27281B] border-solid px-8 py-2 cursor-pointer transition ease-in-out 
           font-[open-sans] font-semibold text-lg'>Let's Talk</button>
             </div>
         </div>
